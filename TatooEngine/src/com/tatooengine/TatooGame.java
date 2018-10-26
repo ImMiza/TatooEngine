f05984d3d9c7d117368a68935b520de7f2fafd3b
AymegikeLewok <aymeric.henouille@gmail.com>
2018-10-26 17:57:39 +0200

    test

--- TatooEngine/src/com/tatooengine/TatooGame.java
+++ TatooEngine/src/com/tatooengine/TatooGame.java
@@ -6,8 +6,7 @@ public class TatooGame { //Main class to import in the main class of your game
 	
 	private static TatooLog tatooLog = new TatooLog(true);
 	
-	public static void main(String[] arg) {
-	}
+	public static void main(String[] arg) {}
 	
 	public static TatooLog getMainTatooLog() {
 		return tatooLog;
